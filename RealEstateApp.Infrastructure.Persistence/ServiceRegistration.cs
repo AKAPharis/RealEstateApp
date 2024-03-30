60edabb2ad7e3fd6295b7dccbfa7d730ff635db8
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-03-30 05:41:08 -0400

    Adding ServiceRegistration in infrastructure layer

---
+++ RealEstateApp.Infrastructure.Persistence/ServiceRegistration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RealEstateApp.Core.Application.Interfaces.Repositories;
+using RealEstateApp.Infrastructure.Persistence.Contexts;
+using RealEstateApp.Infrastructure.Persistence.Repositories;
+using System.Runtime.CompilerServices;
+
+namespace RealEstateApp.Infrastructure.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Contexts
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationContext>(options => options.UseInMemoryDatabase("RealEstateDB"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationContext>(options =>
+                {
+                    options.EnableSensitiveDataLogging();
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                        m => m.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName));
+                });
+            }
+            #endregion
+            #region Repositories
+            services.AddTransient<IPropertyUpgradeRepository, PropertyUpgradeRepository>();
+            services.AddTransient<IRealEstatePropertyRepository, RealEstatePropertyRepository>();
+            services.AddTransient<ITypeOfSaleRepository, TypeOfSaleRepository>();
+            services.AddTransient<ITypePropertyRepository, TypePropertyRepository>();
+            services.AddTransient<IUpgradeRepository, UpgradeRepository>();
+
+            #endregion
+        }
+    }
+}
