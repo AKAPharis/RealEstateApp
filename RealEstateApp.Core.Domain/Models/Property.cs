4bb3aae572ffe6bbf5421203558c36ee563c24c0
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-03-30 04:23:29 -0400

    Add project files.

---
+++ RealEstateApp.Core.Domain/Models/Property.cs
@@ -0,0 +1,21 @@
+﻿namespace RealEstateApp.Core.Domain.Models
+{
+    public class Property
+    {
+        public string Guid { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double Size { get; set; }
+        public int NumberOfBedrooms { get; set; }
+        public int NumberOfBathrooms { get; set; }
+        public List<string> Images { get; set; }
+        public string AgentId { get; set; }
+        public string AgentName { get; set; }
+        public List<Upgrades> Upgrades { get; set; }
+        public TypeOfSale TypeOfSale { get; set; }
+        public TypeProperty TypeProperty { get; set; }
+
+
+    }
+
+}
---
+++ RealEstateApp.Core.Domain/Models/Upgrades.cs
@@ -0,0 +1,10 @@
+﻿using RealEstateApp.Core.Domain.Common;
+
+namespace RealEstateApp.Core.Domain.Models
+{
+    public class Upgrades : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
