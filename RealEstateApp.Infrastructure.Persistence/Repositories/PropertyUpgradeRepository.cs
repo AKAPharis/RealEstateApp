dcd20f6d646a30e57484b5e287bc55146ca42e94
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-03-30 05:32:17 -0400

    Adding general repositories

---
+++ RealEstateApp.Infrastructure.Persistence/Repositories/PropertyUpgradeRepository.cs
@@ -0,0 +1,13 @@
+﻿using RealEstateApp.Core.Application.Interfaces.Repositories;
+using RealEstateApp.Core.Domain.Models;
+using RealEstateApp.Infrastructure.Persistence.Contexts;
+
+namespace RealEstateApp.Infrastructure.Persistence.Repositories
+{
+    public class PropertyUpgradeRepository : GenericRepository<PropertyUpgrade>, IPropertyUpgradeRepository
+    {
+        public PropertyUpgradeRepository(ApplicationContext context) : base(context)
+        {
+        }
+    }
+}
