4ec4d3453bb09a271f07b5dc46780cb8d6abbccf
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-03-31 05:05:23 -0400

    Adding Users ViewModels

---
+++ RealEstateApp.Core.Application/ViewModels/Customer/CustomerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateApp.Core.Application.ViewModels.Customer
+{
+    public class CustomerViewModel
+    {
+        public string? Id { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DocumentId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? UserImagePath { get; set; }
+        public List<string> Roles { get; set; }
+        public bool IsActived { get; set; }
+
+
+    }
+}
---
+++ RealEstateApp.Core.Application/ViewModels/InternalUser/InternalUserViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace RealEstateApp.Core.Application.ViewModels.InternalUser
+{
+    public class InternalUserViewModel
+    {
+        public string? Id { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DocumentId { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<string> Roles { get; set; }
+        public bool IsActived { get; set; }
+
+    }
+}
