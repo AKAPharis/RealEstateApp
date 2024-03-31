43f3cded7da3c725b60ba1f1fe424d84b024bc34
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-03-31 05:04:34 -0400

    Adding the Identity Users

---
+++ RealEstateApp.Infrastructure.Identity/Models/InternalUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RealEstateApp.Infrastructure.Identity.Models
+{
+    public class InternalUser : IdentityUser
+    {
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public string DocumentId { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
