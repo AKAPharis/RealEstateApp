414f485d0a4759b8e5551c901c1d0c56f14ce6b4
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-03-31 05:06:29 -0400

    Adding Identity Contexts

---
+++ RealEstateApp.Infrastructure.Identity/Contexts/InternalUserContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RealEstateApp.Infrastructure.Identity.Models;
+
+namespace RealEstateApp.Infrastructure.Identity.Contexts
+{
+    public class InternalUserContext : IdentityDbContext<InternalUser>
+    {
+        public InternalUserContext(DbContextOptions<InternalUserContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //FLUENT API
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<InternalUser>().ToTable("InternalUsers");
+            modelBuilder.Entity<IdentityRole>().ToTable("InternalRoles");
+            modelBuilder.Entity<IdentityUserRole<string>>().ToTable("InternalUserRoles");
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("InternalUserLogins");
+            modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("InternalRoleClaims");
+            modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("AspNetInternalUserClaims");
+            modelBuilder.Entity<IdentityUserToken<string>>().ToTable("AspNetInternalUserTokens");
+
+
+
+
+        }
+
+    }
+}
