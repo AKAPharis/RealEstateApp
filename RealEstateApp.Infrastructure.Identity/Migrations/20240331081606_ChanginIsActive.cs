812562b3bb26070445a41bc758350dc07b9f4de1
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-03-31 05:10:20 -0400

    Adding Identity Migrations

---
+++ RealEstateApp.Infrastructure.Identity/Migrations/20240331081606_ChanginIsActive.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateApp.Infrastructure.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChanginIsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "InternalUsers",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IsActive",
+                table: "InternalUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
