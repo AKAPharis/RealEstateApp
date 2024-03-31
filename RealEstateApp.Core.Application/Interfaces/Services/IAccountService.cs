3fe01eb1aa8c1ad4259c770493ce1205c19840b4
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-03-31 05:05:59 -0400

    Adding the service interfaces

---
+++ RealEstateApp.Core.Application/Interfaces/Services/IAccountService.cs
@@ -0,0 +1,45 @@
+﻿using RealEstateApp.Core.Application.Dtos.Account.Generals;
+
+namespace RealEstateApp.Core.Application.Interfaces.Services
+{
+    public interface IAccountService
+        <TSaveViewModel,
+        TViewModel,
+        TAuthtenticationResponse,
+        TRegisterResponse,
+        TRegisterRequest,
+        TEditResponse,
+        TEditRequest> //no soy dariana, pero eso ta feo
+        where TSaveViewModel : class
+        where TViewModel : class
+        where TAuthtenticationResponse : class
+        where TRegisterResponse : class
+        where TRegisterRequest : class
+        where TEditResponse : class
+        where TEditRequest : class
+
+
+
+    {
+        Task<TViewModel> GetByIdAsync(string id);
+        Task<TSaveViewModel> GetByIdSaveViewModelAsync(string id);
+        Task<TViewModel> GetByUsernameAsync(string username);
+        Task<TAuthtenticationResponse> AuthenticateAsync(AuthenticationRequest request);
+        Task<string> ConfirmAccountAsync(string userId, string token);
+        Task<ForgotPasswordResponse> ForgotPasswordAsync(ForgotPasswordRequest request, string origin);
+        Task<ResetPasswordResponse> ResetPasswordAsync(ResetPasswordRequest request);
+        Task<TRegisterResponse> RegisterUserAsync(TRegisterRequest request, string origin);
+        Task<TEditResponse> EditUserAsync(TEditRequest request, string origin);
+        Task<List<TViewModel>> GetAll();
+        Task SignOutAsync();
+
+        Task<int> GetActiveUsers();
+
+        Task<int> GetInactiveUsers();
+        Task DeactivateUser(string id);
+
+        Task ActivateUser(string id);
+
+
+    }
+}
---
+++ RealEstateApp.Core.Application/Interfaces/Services/IInternalUserService.cs
@@ -0,0 +1,15 @@
+﻿using RealEstateApp.Core.Application.Dtos.Account.InternalUser;
+using RealEstateApp.Core.Application.ViewModels.InternalUser;
+
+namespace RealEstateApp.Core.Application.Interfaces.Services
+{
+    public interface IInternalUserService : IAccountService<InternalUserSaveViewModel,
+        InternalUserViewModel,
+        InternalUserAuthenticationResponse,
+        InternalUserRegisterResponse,
+        InternalUserRegisterRequest,
+        InternalUserEditResponse,
+        InternalUserEditRequest>
+    {
+    }
+}
