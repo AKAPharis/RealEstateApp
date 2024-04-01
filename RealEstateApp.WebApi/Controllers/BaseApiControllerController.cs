7decd3684c57041c5e434a40a8f92ababc7725b3
Waniel Torres <alexk7320@gmail.com>
2024-04-01 08:53:00 -0500

    Add base config Api

---
+++ RealEstateApp.WebApi/Controllers/BaseApiControllerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateApp.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class BaseApiControllerController : ControllerBase
+    {
+        
+    }
+}
