da6f2595233d65cc9e7197bc11f2d50316280e9d
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-04-04 04:02:50 -0400

    Creating mapping in the Identity layer

---
+++ RealEstateApp.Infrastructure.Identity/Mappings/GeneralProfile.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using RealEstateApp.Core.Application.Dtos.Account.Generals;
+using RealEstateApp.Core.Application.ViewModels.Account;
+using RealEstateApp.Core.Application.ViewModels.Customer;
+using RealEstateApp.Core.Application.ViewModels.InternalUser;
+using RealEstateApp.Infrastructure.Identity.Models;
+
+namespace RealEstateApp.Infrastructure.Identity.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile() 
+        {
+
+            #region Customer
+
+            CreateMap<Customer, CustomerViewModel>()
+                .ReverseMap();
+
+            CreateMap<Customer, CustomerSaveViewModel>()
+                .ReverseMap();
+
+
+
+            #endregion
+            #region InternalUser
+
+            CreateMap<InternalUser, InternalUserViewModel>()
+                .ReverseMap();
+
+            CreateMap<InternalUser, InternalUserSaveViewModel>()
+                .ReverseMap();
+
+            CreateMap<AuthenticationRequest, LoginViewModel>()
+                .ForMember(x => x.HasError, opt => opt.Ignore())
+                .ForMember(x => x.Error, opt => opt.Ignore())
+                .ReverseMap();
+
+            #endregion
+
+
+        }
+
+
+
+    }
+}
