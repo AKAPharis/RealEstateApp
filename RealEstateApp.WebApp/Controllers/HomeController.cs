f1cf2813d9e4d1d18dd596977e20cd4131901a49
Dariana Cabreja <darianacabreja@gmail.com>
2024-04-10 02:05:50 -0400

    Agents, and Property Details

--- RealEstateApp.WebApp/Controllers/HomeController.cs
+++ RealEstateApp.WebApp/Controllers/HomeController.cs
@@ -23,10 +23,9 @@ namespace RealEstateApp.WebApp.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult PropertyDetails()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
