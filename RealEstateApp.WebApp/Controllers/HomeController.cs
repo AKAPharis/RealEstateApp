98e33d17ca18afbbb5fe0ac746a6045291219188
Dariana Cabreja <darianacabreja@gmail.com>
2024-04-09 21:18:53 -0400

    Other designs

--- RealEstateApp.WebApp/Controllers/HomeController.cs
+++ RealEstateApp.WebApp/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace RealEstateApp.WebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Agents()
         {
             return View();
         }
