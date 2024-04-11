7a0bc36142474d4be9ff72a5d125f99a57a077f5
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-04-11 00:38:04 -0400

    Adding Repositories interfaces

---
+++ RealEstateApp.Core.Application/Interfaces/Repositories/IFavoritePropertyRepository.cs
@@ -0,0 +1,11 @@
+﻿using RealEstateApp.Core.Domain.Models;
+
+namespace RealEstateApp.Core.Application.Interfaces.Repositories
+{
+    public interface IFavoritePropertyRepository : IGenericRepository<FavoriteProperty>
+    {
+        Task<List<RealEstateProperty>> GetAllByUser(string userId);
+        Task<List<string>> GetAllByProperty(int propertyId);
+
+    }
+}
