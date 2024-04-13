9c75812ca5f2a4040c83dae1aeac5c718827fe2f
Dariana Cabreja <darianacabreja@gmail.com>
2024-04-13 19:34:35 -0400

    Not yet but login almost ready

--- RealEstateApp.WebApp/Controllers/UserController.cs
+++ RealEstateApp.WebApp/Controllers/UserController.cs
@@ -1,12 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Core.Application.Dtos.Account;
+using RealEstateApp.Core.Application.Interfaces.Services;
+using RealEstateApp.Core.Application.ViewModels.Account;
+using RealEstateApp.Core.Application.Helpers;
 
 namespace RealEstateApp.WebApp.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUserService _userService;
+        private readonly IHttpContextAccessor _contextAccessor;
+
         public IActionResult Index()
         {
-            return View();
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            AuthenticationResponse userVm = await _userService.AuthenticateAsync(vm);
+            if (userVm != null && userVm.HasError != true)
+            {
+                HttpContext.Session.Set<AuthenticationResponse>("user", userVm);
+
+                if (userVm.Roles.Any(r => r == "Customer"))
+                {
+                    return RedirectToRoute(new { controller = "Home", action = "CustomerHome" });
+                }
+                else if (userVm.Roles.Any(r => r == "RealEstateAgent"))
+                {
+                    return RedirectToRoute(new { controller = "Agent", action = "AgentHome" });
+                }
+                else
+                {
+                    return RedirectToRoute(new { controller = "Administrator", action = "AdminHome" });
+                }
+            }
+            else
+            {
+                vm.HasError = userVm.HasError;
+                vm.Error = userVm.Error;
+                return View(vm);
+            }
+
         }
     }
 }
