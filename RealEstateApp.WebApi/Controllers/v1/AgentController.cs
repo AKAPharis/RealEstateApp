eb8f4cf458b3085d402e8d996e9c1d956bc8920f
Waniel Torres <alexk7320@gmail.com>
2024-04-14 14:05:55 -0400

    Agent Controller - blank -

---
+++ RealEstateApp.WebApi/Controllers/v1/AgentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace RealEstateApp.WebApi.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Authorize]
+    [SwaggerTag("Mantenimientos de agentes")]
+    public class AgentController : BaseApiController
+    {
+        
+    }
+}
