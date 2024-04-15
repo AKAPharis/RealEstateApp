9227dae7fe3faf27e1c994071ec974028a05e914
Dariana Cabreja <darianacabreja@gmail.com>
2024-04-15 17:12:57 -0400

    Adding functions: Upgrade, Types

--- RealEstateApp.WebApp/Controllers/AdministratorController.cs
+++ RealEstateApp.WebApp/Controllers/AdministratorController.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Core.Application.Enums.Roles;
+using RealEstateApp.Core.Application.Interfaces.Services;
 
 namespace RealEstateApp.WebApp.Controllers
 {
     public class AdministratorController : Controller
     {
-        public IActionResult Index()
+        private readonly IUserService _userService;
+
+        public AdministratorController(IUserService userService)
         {
-            return View();
+            _userService = userService;
         }
+
+        public async Task<IActionResult> Index() => View(await _userService.GetAllByRoleViewModel(nameof(UserRoles.Admin)));
 
         public IActionResult AdminHome()
         {
--- RealEstateApp.WebApp/Controllers/PropertyTypeController.cs
+++ RealEstateApp.WebApp/Controllers/PropertyTypeController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Core.Application.Interfaces.Services;
+using RealEstateApp.Core.Application.ViewModels.TypeOfProperty;
 
 namespace RealEstateApp.WebApp.Controllers
 {
     public class PropertyTypeController : Controller
     {
-        public IActionResult Index()
+        private readonly ITypeOfPropertyService _typeOfPropertyService;
+
+        public PropertyTypeController(ITypeOfPropertyService typeOfPropertyService)
         {
-            return View();
+            _typeOfPropertyService = typeOfPropertyService;
         }
+
+        public async Task<IActionResult> Index() => View(await _typeOfPropertyService.GetAllAsync());
+
+        public IActionResult Create() => View("SavePropertyType", new SaveTypeOfPropertyViewModel());
+        
+        [HttpPost]
+        public async Task<IActionResult> Create(SaveTypeOfPropertyViewModel vm)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("SavePropertyType",vm);
+            }
+            await _typeOfPropertyService.CreateAsync(vm);
+            return RedirectToAction("Index");
+        }
+
     }
 }
