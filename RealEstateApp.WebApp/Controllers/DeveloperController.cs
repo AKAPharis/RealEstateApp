f4364ab4e1f6d1cc8d96f8b10e1bb211941cbe9b
Dariana Cabreja <darianacabreja@gmail.com>
2024-04-17 05:37:49 -0400

    Adding developer view

--- RealEstateApp.WebApp/Controllers/DeveloperController.cs
+++ RealEstateApp.WebApp/Controllers/DeveloperController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Core.Application.Enums.Roles;
+using RealEstateApp.Core.Application.Interfaces.Services;
 
 namespace RealEstateApp.WebApp.Controllers
 {
     public class DeveloperController : Controller
     {
-        public IActionResult Index()
+        private readonly IUserService _userService;
+
+        public DeveloperController(IUserService userService)
         {
-            return View();
+            _userService = userService;
         }
+
+        public async Task<IActionResult> Index() => View(await _userService.GetAllByRoleViewModel(nameof(UserRoles.Admin)));
+
     }
 }
