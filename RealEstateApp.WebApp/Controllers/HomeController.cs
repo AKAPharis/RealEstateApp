b6f93816a795e55d04f913d83f09ced90f082a0b
Dariana Cabreja <darianacabreja@gmail.com>
2024-04-18 02:03:10 -0400

    Amost ready customer home and properties

--- RealEstateApp.WebApp/Controllers/HomeController.cs
+++ RealEstateApp.WebApp/Controllers/HomeController.cs
@@ -10,11 +10,11 @@ namespace RealEstateApp.WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IRealEstatePropertyService _propertyService;
-        private readonly ITypeOfPropertyService _typeOfProperty;
+        private readonly IMapper _mapper;
         private readonly IUserService _userService;
         private readonly IHttpContextAccessor _contextAccessor;
-        private readonly IMapper _mapper;
+        private readonly ITypeOfPropertyService _typeOfProperty;
+        private readonly IRealEstatePropertyService _propertyService;
         private readonly IFavoritePropertyService _favoritePropertyService;
 
         public HomeController(IRealEstatePropertyService propertyService, IUserService userService, IHttpContextAccessor contextAccessor, IMapper mapper, ITypeOfPropertyService typeOfProperty, IFavoritePropertyService favoritePropertyService)
@@ -39,12 +39,11 @@ namespace RealEstateApp.WebApp.Controllers
         {
             var agents = await _userService.GetAgentByNameAsync(agentName);
             return View("Agents", agents);
-
         }
 
         public async Task<IActionResult> PropertyDetails(int Id)
         {
-            RealStatePropertyDetailsViewModel property = new();
+            RealEstatePropertyDetailsViewModel property = new();
             property.Property = await _propertyService.GetAllByIdWithIncludeAsync(Id);
             if (property.Property != null)
             {
@@ -63,7 +62,11 @@ namespace RealEstateApp.WebApp.Controllers
         public async Task<IActionResult> CustomerHome(RealEstatePropertyFilterViewModel filter)
         {
             ViewBag.TypeOfProperty = await _typeOfProperty.GetAllAsync();
-            return View(await _propertyService.GetAllByFilter(filter));
+            var properties = await _propertyService.GetAllByFilter(filter);
+            RealEstatePropertyCustomerViewModel list = new();
+            list.Properties = properties;
+            list.FavoriteProperties = await _favoritePropertyService.GetAllPropertyIdByUser(_contextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user").Id);
+            return View(list);
         }
 
         public async Task<IActionResult> AddFavoriteProperty(int Id)
