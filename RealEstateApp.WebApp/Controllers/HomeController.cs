a13abcad43a83622e8afb6e0222309845fd71084
Dariana Cabreja <darianacabreja@gmail.com>
2024-04-18 01:09:26 -0400

    Adding customer home

--- RealEstateApp.WebApp/Controllers/HomeController.cs
+++ RealEstateApp.WebApp/Controllers/HomeController.cs
@@ -1,12 +1,10 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Core.Application.Helpers;
 using RealEstateApp.Core.Application.Enums.Roles;
-using RealEstateApp.Core.Application.Interfaces.Repositories;
+using RealEstateApp.Core.Application.Dtos.Account;
 using RealEstateApp.Core.Application.Interfaces.Services;
-using RealEstateApp.Core.Application.ViewModels.Account;
 using RealEstateApp.Core.Application.ViewModels.RealEstateProperty;
-using RealEstateApp.WebApp.Models;
-using System.Diagnostics;
 
 namespace RealEstateApp.WebApp.Controllers
 {
@@ -17,22 +15,24 @@ namespace RealEstateApp.WebApp.Controllers
         private readonly IUserService _userService;
         private readonly IHttpContextAccessor _contextAccessor;
         private readonly IMapper _mapper;
+        private readonly IFavoritePropertyService _favoritePropertyService;
 
-        public HomeController(IRealEstatePropertyService propertyService, IUserService userService, IHttpContextAccessor contextAccessor, IMapper mapper, ITypeOfPropertyService typeOfProperty)
+        public HomeController(IRealEstatePropertyService propertyService, IUserService userService, IHttpContextAccessor contextAccessor, IMapper mapper, ITypeOfPropertyService typeOfProperty, IFavoritePropertyService favoritePropertyService)
         {
             _propertyService = propertyService;
             _userService = userService;
             _contextAccessor = contextAccessor;
             _mapper = mapper;
             _typeOfProperty = typeOfProperty;
+            _favoritePropertyService = favoritePropertyService;
         }
 
         public async Task<IActionResult> Index(RealEstatePropertyFilterViewModel filter)
         {
             ViewBag.TypeOfProperty = await _typeOfProperty.GetAllAsync();
-            return View(await _propertyService.GetAllByFilter(filter)); 
+            return View(await _propertyService.GetAllByFilter(filter));
         }
-       
+
         public async Task<IActionResult> Agents() => View(await _userService.GetAllByRoleViewModel(nameof(UserRoles.RealEstateAgent)));
 
         public async Task<IActionResult> PropertyDetails(int Id)
@@ -53,9 +53,30 @@ namespace RealEstateApp.WebApp.Controllers
             return RedirectToAction("PropertyDetails", property.Id);
         }
 
-        public IActionResult CustomerHome()
+        public async Task<IActionResult> CustomerHome(RealEstatePropertyFilterViewModel filter)
         {
-            return View();
+            ViewBag.TypeOfProperty = await _typeOfProperty.GetAllAsync();
+            return View(await _propertyService.GetAllByFilter(filter));
+        }
+
+        public async Task<IActionResult> AddFavoriteProperty(int Id)
+        {
+            CreateFavoritePropertyViewModel favProperty = new()
+            {
+                PropertyId = Id,
+                UserId = _contextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user").Id
+            };
+            var response = await _favoritePropertyService.CreateFavoriteAsync(favProperty);
+            if (response.HasError)
+            {
+                ViewBag.CreateResponse = new
+                {
+                    response.Error,
+                    hasError = response.HasError
+                };
+                return RedirectToAction("CustomerHome");
+            }
+            return RedirectToAction("CustomerHome");
         }
     }
 }
